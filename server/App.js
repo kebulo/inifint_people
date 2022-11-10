8dff50e078df7a9e01d16f48249db9fba4f24cae
Kebulo <kbuiles@lendingfront.com>
2022-11-10 09:34:59 -0500

    Remove routes unnecesery and added new command to package json

--- server/App.js
+++ server/App.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const session = require('express-session');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -12,9 +11,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.resolve(__dirname, '../client/build')));
-app.use(session({secret: 'ssshhhhh',saveUninitialized: true,resave: true}));
 
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
